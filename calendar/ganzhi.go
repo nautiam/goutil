5d03357bd6789dc15d4413c3d8a14d845d1569e6
Hoang Anh Tuan <anhtuan0105@gmail.com>
2025-07-22 23:52:18 +0700

    Update Vietnamese GanZhi

--- calendar/ganzhi.go
+++ calendar/ganzhi.go
@@ -6,9 +6,9 @@ import (
 
 var (
 	// 1900 2月春分后为 庚子年
-	GanTable    = []string{"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"}
-	ZhiTable    = []string{"子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"}
-	AnimalTable = []string{"鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊", "猴", "鸡", "狗", "猪"}
+	GanTable    = []string{"Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ", "Canh", "Tân", "Nhâm", "Quý"}
+	ZhiTable    = []string{"Tý", "Sửu", "Dần", "Mão", "Thìn", "Tỵ", "Ngọ", "Mùi", "Thân", "Dậu", "Tuất", "Hợi"}
+	AnimalTable = []string{"Chuột", "Trâu", "Hổ", "Mèo", "Rồng", "Rắn", "Ngựa", "Dê", "Khỉ", "Gà", "Chó", "Lợn"}
 )
 
 // Tian Gan
@@ -39,19 +39,19 @@ func GanZhiYMD(sYear, sMonth, sDay int) (gzYear, gzMonth, gzDay string) {
 		gzYear = GanZhi(sYear - MinYear + 36)
 	}
 
-	//立春日期
+	// 立春日期
 	jq3 := JieQi(sYear, 3)
 
-	//月柱 1900年1月小寒以前为 丙子月(60进制12)
-	firstNode := JieQi(sYear, sMonth*2-1) //返回当月「节」为几日开始
+	// 月柱 1900年1月小寒以前为 丙子月(60进制12)
+	firstNode := JieQi(sYear, sMonth*2-1) // 返回当月「节」为几日开始
 	gzMonth = GanZhi((sYear-MinYear)*12 + sMonth + 11)
 
-	//依节气调整二月分的年柱, 以立春为界
+	// 依节气调整二月分的年柱, 以立春为界
 	if sMonth == 2 && sDay >= jq3 {
 		gzYear = GanZhi(sYear - MinYear + 36)
 	}
 
-	//依节气月柱, 以「节」为界
+	// 依节气月柱, 以「节」为界
 	if sDay >= firstNode {
 		gzMonth = GanZhi((sYear-MinYear)*12 + sMonth + 12)
 	}
@@ -62,7 +62,7 @@ func GanZhiYMD(sYear, sMonth, sDay int) (gzYear, gzMonth, gzDay string) {
 	now2 := time.Date(sYear, time.Month(sMonth), 1, 0, 0, 0, 0, time.UTC)
 	offset := int(now2.Sub(base2).Seconds()/86400) + 10
 
-	//日柱
+	// 日柱
 	gzDay = GanZhi(offset + sDay - 1)
 
 	return
